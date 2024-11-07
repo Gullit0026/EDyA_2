e857296af909321fd4123fc0eff291748f04627c
Gullit0026 <juliansalamancaa@gmail.com>
2024-11-06 20:06:30 -0500

    challenge react 6

---
+++ challenge6/src/App.jsx
@@ -0,0 +1,30 @@
+import { useFetch } from "./hook/useFetch";
+import { useState, useEffect } from "react";
+
+export function App() {
+
+  const { data, isLoading, hasError } = useFetch(
+    "https://pokeapi.co/api/v2/pokemon/"
+  );
+
+  const [theData, setTheData] = useState(data);
+
+  useEffect(() => {
+    setTheData(data);
+  }, [data]);
+
+  return (
+    <>
+      <h1>Data:</h1>
+      <section>
+        {isLoading ? (
+          <span>Loading...</span>
+        ) : hasError ? (
+          <span>Error fetching data</span>
+        ) : (
+          <span>{JSON.stringify(theData)}</span>
+        )}
+      </section>
+    </>
+  );
+}
